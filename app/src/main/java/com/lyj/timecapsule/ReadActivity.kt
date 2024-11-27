c6399aa7fa8cf813087d30625c63df6845f09cae
LYJ22 <pippi478@gmail.com>
2024-11-27 17:37:29 +0900

    [create] create project

---
+++ app/src/main/java/com/lyj/timecapsule/ReadActivity.kt
@@ -0,0 +1,14 @@
+package com.lyj.timecapsule
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ReadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_read)
+    }
+}
