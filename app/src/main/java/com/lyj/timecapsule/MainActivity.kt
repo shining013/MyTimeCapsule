b518c58bafdd82569d3c40d229ad31da6a3eb2d5
chaelin2 <gcl693427@gmail.com>
2024-11-28 14:53:46 +0900

    ãdetail

--- app/src/main/java/com/lyj/timecapsule/MainActivity.kt
+++ app/src/main/java/com/lyj/timecapsule/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.lyj.timecapsule
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            val intent  = Intent(this, ReadActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
