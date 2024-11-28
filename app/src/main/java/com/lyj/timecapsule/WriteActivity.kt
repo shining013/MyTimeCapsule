0ec14d2bd09eab7927a2bf64eeebedcd5018fcd5
LYJ22 <yj9977@dgu.ac.kr>
2024-11-28 12:43:58 +0900

    [feat] 작성 페이지 layout

--- app/src/main/java/com/lyj/timecapsule/WriteActivity.kt
+++ app/src/main/java/com/lyj/timecapsule/WriteActivity.kt
@@ -1,6 +1,9 @@
 package com.lyj.timecapsule
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TimePicker
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +13,13 @@ class WriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write)
+
+        var title = findViewById<EditText>(R.id.editTitleText_write)
+        var content = findViewById<EditText>(R.id.editContentText_write)
+        var calButton = findViewById<Button>(R.id.buttonCal)
+        var time = findViewById<TimePicker>(R.id.time_picker)
+        var share = findViewById<EditText>(R.id.editShareText_write)
+
+
     }
 }
