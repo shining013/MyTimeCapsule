a050a576855601cece58f300ea136419991f3581
shining013 <nevertheless113@naver.com>
2024-11-28 15:56:44 +0900

    feat:list

--- app/src/main/java/com/lyj/timecapsule/ListActivity.kt
+++ app/src/main/java/com/lyj/timecapsule/ListActivity.kt
@@ -2,8 +2,10 @@ package com.lyj.timecapsule
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class ListActivity : AppCompatActivity() {
@@ -14,6 +16,28 @@ class ListActivity : AppCompatActivity() {
         findViewById<Button>(R.id.buttonShow).setOnClickListener {
             val intent = Intent(this, ReadActivity::class.java)
             startActivity(intent);
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val listView: ListView = findViewById(R.id.listView)
+
+        val list = SharedData.capsuleList.map {it.title}
+        // ArrayAdapter 설정
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list)
+
+        // ListView에 Adapter 연결
+        listView.adapter = adapter
+
+        listView.setOnItemClickListener(){parent, view, position, id  ->
+            val selectedItem = list[position]
+
+            val intent = Intent(this, ReadActivity::class.java).apply {
+                putExtra("title",selectedItem)
+            }
+            startActivity(intent)
         }
     }
 }
