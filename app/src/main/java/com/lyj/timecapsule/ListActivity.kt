bea16cdce3a6e05b729d2b52289750720dec3ab1
shining013 <nevertheless113@naver.com>
2024-11-28 14:42:41 +0900

    main page

---
+++ app/src/main/java/com/lyj/timecapsule/ListActivity.kt
@@ -0,0 +1,13 @@
+package com.lyj.timecapsule
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
